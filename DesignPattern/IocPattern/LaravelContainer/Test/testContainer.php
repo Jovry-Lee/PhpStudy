004ada1058272553a85d3047a33725fe90585078
李枚娟 <limj@weipaitang.com>
2022-02-18 11:06:07 +0800

    update

--- DesignPattern/IocPattern/LaravelContainer/Test/testContainer.php
+++ DesignPattern/IocPattern/LaravelContainer/Test/testContainer.php
@@ -1,15 +1,16 @@
 <?php
 
-require_once __DIR__ . "/../../../init.php";
-
 use IocPattern\LaravelContainer\Container;
 use IocPattern\LaravelContainer\Test\Traffic\Train;
+use IocPattern\LaravelContainer\Test\Traffic\Visit;
 use IocPattern\LaravelContainer\Test\Traveller;
+
+require_once __DIR__ . "/../../../init.php";
 
 // 实例化容器。
 $app = new Container();
 // 绑定
-$app->bind('Visit', Train::class);
+$app->bind(Visit::class, Train::class);
 $app->bind('Traveller', Traveller::class);
 
 // 通过容器实现依赖注入，完成类的实例化。
